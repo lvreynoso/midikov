4c38dc3dddf2fe3b7dd437bcfab3a6b65ef2301c
Lucia Reynoso <lvreynoso@protonmail.com>
2019-01-10 00:28:15 -0800

    play pause stop button code

--- public/scripts/mimikov.js
+++ public/scripts/mimikov.js
@@ -5,9 +5,22 @@ const player = new Timidity('scripts/')
 player.load('../midi/Legend of Zelda - Overworld.mid')
 window.onload = function() {
     document.getElementById('playButton').addEventListener('click', playButton)
+    document.getElementById('pauseButton').addEventListener('click', pauseButton)
+    document.getElementById('stopButton').addEventListener('click', stopButton)
 }
 
 function playButton(event) {
     player.play()
     console.log('Play me!');
+}
+
+function pauseButton(event) {
+    player.pause()
+    console.log('Pause me!');
+}
+
+function stopButton(event) {
+    player.pause()
+    player.seek(0)
+    console.log('Stop me!');
 }
